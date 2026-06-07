6101979ac8433d1c118cb895c64a079f1488324d
Hariharan Thavachelvam <164553783+thavaahariharangit@users.noreply.github.com>
2026-06-07 20:59:21 +0000

    import

---
+++ 29_imports/code.py
@@ -0,0 +1,12 @@
+# from mymodule import divide
+
+# print("code.py: ", __name__)
+# print("Result of divide(10, 2): ", divide(10, 2))
+
+# import sys
+
+# print(sys.path)
+
+import mymodule
+
+print("code.py: ", __name__)
---
+++ 29_imports/mymodule.py
@@ -0,0 +1,7 @@
+# def divide(dividend, divisor):
+#     return dividend / divisor
+
+import lib.mylib
+
+print("mymodule.py: ", __name__)
+
