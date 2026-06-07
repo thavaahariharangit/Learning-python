fc52a327b583f031ee9a1fbd69f81efe67eda825
Hariharan Thavachelvam <164553783+thavaahariharangit@users.noreply.github.com>
2026-06-07 09:41:52 +0000

    functions

---
+++ 15_functions/code.py
@@ -0,0 +1,18 @@
+# def hello():
+#     print("Hello!")
+
+# hello()
+
+# def user_age_seconds():
+#     user_age = int(input("age: "))
+#     age_seconds = user_age * 365 * 24 * 60 * 60
+#     print(f"age in second: {age_seconds}")
+
+# user_age_seconds()
+
+def add_friend():
+    friends.append("Rolf")
+
+friends = []
+add_friend()
+print(friends)
